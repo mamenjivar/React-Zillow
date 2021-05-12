a728cce836663eddb9f5d276fd03e254ee28d0cd
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-05-12 11:28:01 -0700

    added the new feature and got property to zoom into property of choice

--- zillow-react-esp/src/components/View.js
+++ zillow-react-esp/src/components/View.js
@@ -5,6 +5,11 @@ import { Card, Button, CardDeck } from 'react-bootstrap';
 // * Will just show listings of all properties
 // * Map -> View component
 const View = (props) => {
+    // pass view property info up
+    const onClickHandler = (lat, long) => {
+        props.panTo({ lat: lat, lng: long})
+    }
+
     return (
         <div>
             <CardDeck>
@@ -13,10 +18,13 @@ const View = (props) => {
                         <Card.Title>{props.location}</Card.Title>
                         <Card.Text>
                                 <p>Name: {props.name}</p>
-                                <p>Price: {props.price}</p>
+                                <p>Price: ${props.price}</p>
                         </Card.Text>
-                        <Button variant="outline-primary">
-                            View Property
+                        <Button 
+                            variant="outline-primary"
+                            onClick={() => onClickHandler(props.lat, props.long)}
+                        >
+                            Map It
                         </Button>
                     </Card.Body>
                 </Card>
